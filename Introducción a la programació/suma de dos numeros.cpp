73c9ff1b0fc3342327f58acea4f52762c3aee89b
Stefania Oliva Caipe <93621268+tefa031@users.noreply.github.com>
2023-11-16 02:10:46 +0000

    Se agrega la solucion de dos números

---
+++ Introducción a la programació/suma de dos numeros.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+using namespace std;
+
+int main(int argc, char** argv) {
+	int A,B,S;
+	cout<<"A=: ";
+	cin>>A;
+	cout<<"B=: ";
+	cin>>B;
+	 if(A>0 && B>0){
+                 cout<<A+B<<endl;
+	     system ("pause");
+           }
+
+	return 0;
+}
